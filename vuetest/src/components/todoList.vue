fd03aec233f371eba441ee8ac53f350a71bcbad0
yangqiyan <qiyan.yang@cm-dt.com>
2017-07-25 17:38:55 +0800

    同步

--- vuetest/src/components/todoList.vue
+++ vuetest/src/components/todoList.vue
@@ -1,19 +1,20 @@
 <template>
-    <div>
-        <div v-for='item in data' @click='say'>{{item}}</div>
-        <el-dropdown @command="handleCommand">
-            <span class="el-dropdown-link">
+<div>
+    <div v-for='item in data' @click='say'>{{item}}</div>
+    <el-dropdown @command="handleCommand">
+        <span class="el-dropdown-link">
                 {{typeof select == "string" ?select:'下拉菜单'}}
                 <i class="el-icon-caret-bottom el-icon--right"></i>
             </span>
-            <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item v-for='item1 in data' :command='item1'>{{item1}}</el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>
-        <div @myself = "say">自定义事件</div>
-        <div>这是我引人的文件</div>
-        <html-name-hh></html-name-hh>
-    </div>
+        <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item v-for='item1 in data' :command='item1'>{{item1}}</el-dropdown-item>
+        </el-dropdown-menu>
+    </el-dropdown>
+    <div @myself="say">自定义事件</div>
+    <div>这是我引人的文件</div>
+    <html-name-hh></html-name-hh>
+    <div> 这行是用来测试git上传文件的 </div>
+</div>
 </template>
 <script>
 import Vue from 'vue';
@@ -25,16 +26,18 @@ export default {
             select: String
         }
     },
-    components:{htmlNameHh},
+    components: {
+        htmlNameHh
+    },
     props: {
         data: Array,
-        
+
     },
     methods: {
         handleCommand(command) {
             this.select = command
             this.$message('click' + command)
-            this.$emit('myself','1')
+            this.$emit('myself', '1')
         },
         say() {
             alert('zhe')
